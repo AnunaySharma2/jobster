3cd91ed7fd198d856205b101eb5a6c4f3001c934
anuna <anunaysharma123456789@gmail.com>
2023-04-29 12:07:30 +0530

    added insert into supabase in Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -11,10 +11,39 @@ import {
   ModalCloseButton,
 } from "@chakra-ui/react";
 import { useDisclosure } from "@chakra-ui/react";
+import { supabase } from "../supabaseClient";
 
 function Projects() {
   const [projects, setProjects] = useState([]);
+  const [name, setName] = useState("");
+  const [techStack, setTechStack] = useState("");
+  const [desc, setDesc] = useState("");
+  const [link, setLink] = useState("");
+  const [github, setGithub] = useState("");
+  const [email, setEmail] = useState("");
+
+  const session = supabase.auth.getSession();
+  session.then((data) => setEmail(data.data.session.user.email)).catch((err) => console.log(err));
+
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const submitHandler = async () => {
+      const {data, error} = await supabase
+      .from("Projects")
+      .insert([
+        {projectname: name,
+        techstack: techStack,
+        description: desc,
+        livelink: link,
+        github: github,
+        user_email: email}
+      ]);
+      if(data){
+        console.log(data);
+      }else{
+        console.log(error); 
+      }
+  }
 
   return (
     <div className="h-screen p-3">
@@ -31,7 +60,7 @@ function Projects() {
       </div>
       <button
         onClick={onOpen}
-        className="fixed font-bold text-lg bottom-5 right-5 rounded-full bg-blue-500 text-white p-4"
+        className="fixed font-black text-lg bottom-5 right-5 rounded-full bg-blue-500 text-white p-4"
       >
         Add project
       </button>
@@ -47,34 +76,49 @@ function Projects() {
                   type="text"
                   name="name"
                   id="name"
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
                   placeholder="Project Name"
                   className="shadow appearance-none border mb-2 rounded w-full py-2 px-3 text-lightwhite-100 leading-tight focus:outline-none focus:shadow-outline"
                 />
                 <input
                   type="text"
-                  name="name"
-                  id="name"
+                  name="techstack"
+                  id="techstack"
+                  onChange={(e) => {
+                    setTechStack(e.target.value);
+                  }}
                   placeholder="Tech stack used"
                   className="shadow appearance-none border my-2 rounded w-full py-2 px-3 text-lightwhite-100 leading-tight focus:outline-none focus:shadow-outline"
                 />
                 <input
                   type="text"
-                  name="name"
-                  id="name"
+                  name="desc"
+                  id="desc"
+                  onChange={(e) => {
+                    setDesc(e.target.value);
+                  }}
                   placeholder="Brief Description"
                   className="shadow appearance-none border my-2 rounded w-full py-2 px-3 text-lightwhite-100 leading-tight focus:outline-none focus:shadow-outline"
                 />
                 <input
                   type="text"
-                  name="name"
-                  id="name"
+                  name="link"
+                  id="link"
+                  onChange={(e) => {
+                    setLink(e.target.value);
+                  }}
                   placeholder="Live Link"
                   className="shadow appearance-none border my-2 rounded w-full py-2 px-3 text-lightwhite-100 leading-tight focus:outline-none focus:shadow-outline"
                 />
                 <input
                   type="text"
-                  name="name"
-                  id="name"
+                  name="github"
+                  id="github"
+                  onChange={(e) => {
+                    setGithub(e.target.value);
+                  }}
                   placeholder="Github"
                   className="shadow appearance-none border my-2 rounded w-full py-2 px-3 text-lightwhite-100 leading-tight focus:outline-none focus:shadow-outline"
                 />
@@ -82,7 +126,7 @@ function Projects() {
             </form>
           </ModalBody>
           <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
+            <Button colorScheme="blue" mr={3} onClick={submitHandler}>
               Submit
             </Button>
           </ModalFooter>
